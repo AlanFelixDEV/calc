49b1b48fdcc938b9080016dede16e3debb7a510f
Alan <alanfelixds@gmail.com>
2023-04-04 15:00:12 -0300

    retornando um widget

--- lib/calc.dart
+++ lib/calc.dart
@@ -1,7 +1,22 @@
 library calc;
 
+import 'package:flutter/material.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+
+  Widget textformFieldRounded() {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+        enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+        focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+        disabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+        errorBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+        hintText: "pacotes com flutter",
+      ),
+    );
+  }
 }
